66cd32fcc1b7483e61dac5aee2918103aa1438fd
Paulo Henrique <phdsreis@gmail.com>
2025-06-18 02:14:04 -0300

    feat: dia 9

---
+++ projeto/proj02/src/game_functions.py
@@ -0,0 +1,40 @@
+import sys
+import pygame
+
+def check_keydown_events(event, ship):
+    """Responde a eventos de pressionamento de teclas."""
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = True
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = True
+
+def check_keyup_events(event, ship):
+    """Responde a eventos de soltura de teclas."""
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = False
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = False
+
+def check_events(ship):
+    """Responde a eventos de pressionamento de teclas e de mouse."""
+    
+    # Observa eventos de teclado e de mouse.
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+        # Move a nave para a direita ou esquerda quando a tecla direita é pressionada.
+        elif event.type == pygame.KEYDOWN:
+            check_keydown_events(event, ship)
+        # Para a espaçonave quando a tecla é solta.
+        elif event.type == pygame.KEYUP:
+            check_keyup_events(event, ship)
+            
+
+def update_screen(ai_settings, screen, ship):
+    """Atualiza as imagens na tela e alterna para a nova tela."""
+    # Redesenha a tela a cada passagem pelo laço.
+    screen.fill(ai_settings.bg_color)
+    ship.blitme()
+    
+    # Deixa a tela mais recente visível.
+    pygame.display.flip()
